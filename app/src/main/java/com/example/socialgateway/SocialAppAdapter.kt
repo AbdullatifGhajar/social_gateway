3201b3fbad2b68337ab965f748dc26932dd7851f
AbdullatifGhajar <abdullatif.ghajar@gmai.co>
2021-11-04 00:43:10 +0100

    Add onTouch animation to ImageViews

--- app/src/main/java/com/example/socialgateway/SocialAppAdapter.kt
+++ app/src/main/java/com/example/socialgateway/SocialAppAdapter.kt
@@ -1,15 +1,19 @@
 package com.example.socialgateway
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 
+
 class SocialAppAdapter(
     private val context: Context,
     private val onClick: (Context, SocialApp) -> Unit
 ) : BaseAdapter() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val socialApp = SocialApps[position]
         val imageView = convertView as? ImageView ?: ImageView(context).apply {
@@ -18,7 +22,24 @@ class SocialAppAdapter(
 
         return imageView.apply {
             setImageResource(socialApp.imageId)
-            setOnClickListener { onClick(context, socialApp) }
+            scaleX = 0.9f
+            scaleY = 0.9f
+            setOnClickListener {
+                onClick(context, socialApp)
+            }
+            setOnTouchListener { v, event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        v.scaleY = 1f
+                        v.scaleX = 1f
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        v.scaleY = 0.9f
+                        v.scaleX = 0.9f
+                    }
+                }
+                false
+            }
         }
     }
 
