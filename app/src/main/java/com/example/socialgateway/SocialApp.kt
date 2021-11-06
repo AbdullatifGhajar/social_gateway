bde1ef6182bf9d7aa73fb416274286c4c833265d
AbdullatifGhajar <abdullatif.ghajar@gmai.co>
2021-11-06 22:30:09 +0100

    Update list of social apps

--- app/src/main/java/com/example/socialgateway/SocialApp.kt
+++ app/src/main/java/com/example/socialgateway/SocialApp.kt
@@ -7,11 +7,16 @@ class SocialApp(
 )
 
 val SocialApps = listOf(
-    SocialApp("Telegram", "org.telegram.messenger", R.drawable.telegram),
+    SocialApp("Snapchat", "com.snapchat.android", R.drawable.snapchat),
+    SocialApp("TikTok", "com.zhiliaoapp.musically", R.drawable.tiktok),
+    SocialApp("Instagram", "com.instagram.android", R.drawable.instagram),
+    SocialApp("Twitter", "com.twitter.android", R.drawable.twitter),
+    SocialApp("Discord", "com.discord", R.drawable.discord),
+    SocialApp("Reddit", "com.reddit.frontpage", R.drawable.reddit),
     SocialApp("WhatsApp", "com.whatsapp", R.drawable.whatsapp),
     SocialApp("Facebook", "com.facebook.katana", R.drawable.facebook),
     SocialApp("Facebook Messenger", "com.facebook.orca", R.drawable.facebook_messenger),
-    SocialApp("Instagram", "com.instagram.android", R.drawable.instagram),
-    SocialApp("Signal", "org.thoughtcrime.securesms", R.drawable.signal),
-    SocialApp("Snapchat", "com.snapchat.android", R.drawable.snapchat)
+    SocialApp("Youtube", "com.google.android.youtube", R.drawable.youtube)
+    // SocialApp("Telegram", "org.telegram.messenger", R.drawable.telegram),
+    // SocialApp("Signal", "org.thoughtcrime.securesms", R.drawable.signal)
 )
